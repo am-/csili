34cd5f09028a990afc099c93ec15ec783acac7eb
Andre Moelle <andre.moelle@gmail.com>
2016-05-26 19:40:38 +0200

    Refactoring: A tree automaton is now parameterized of its states. Added: Normalization to turn a parameterized tree automaton into a tree automaton with ints as states.

--- src/Csili/Backend/TreeAutomaton.hs
+++ src/Csili/Backend/TreeAutomaton.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Csili.Backend.TreeAutomaton
 (
@@ -17,31 +18,57 @@ import qualified Data.Set as Set
 
 import Csili.Types
 
-data Action
-    = NextState Int
+data Action a
+    = NextState a
     | Apply Rule
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
-data Match
-    = State Int
-    | Everything
-    deriving (Show, Eq)
+data Match a
+    = State a
+    | AnythingDifferent
+    deriving (Show, Eq, Ord)
 
-data TreeAutomaton = TreeAutomaton
-    { delta :: Map Symbol (Map [Match] Action)
+data TreeAutomaton a = TreeAutomaton
+    { delta :: Map Symbol (Map [Match a] (Action a))
     } deriving (Show, Eq)
 
-fromRule :: Rule -> TreeAutomaton
+fromRule :: Rule -> TreeAutomaton a
 fromRule (lhs, rhs) = undefined
 
-alphabet :: TreeAutomaton -> Set Symbol
+alphabet :: TreeAutomaton a -> Set Symbol
 alphabet = Map.keysSet . delta
 
-states :: TreeAutomaton -> IntSet
-states = IntSet.fromList . concatMap (catMaybes . map fromAction . Map.elems) . Map.elems . delta
+states :: Ord a => TreeAutomaton a -> Set a
+states = Set.fromList . concatMap (catMaybes . map fromAction . Map.elems) . Map.elems . delta
   where
-    fromAction :: Action -> Maybe Int
+    fromAction :: Action a -> Maybe a
     fromAction = \case
         NextState s -> Just s
         Apply _ -> Nothing
-    
+
+--------------------------------------------------------------------------------
+-- Normalization
+--------------------------------------------------------------------------------
+
+normalize :: Ord a => TreeAutomaton a -> TreeAutomaton Int
+normalize = flip renameStates <*> Map.fromList . flip zip [0..] . Set.toList . states
+
+renameStates :: (Ord a, Ord b) => Map a b -> TreeAutomaton a -> TreeAutomaton b
+renameStates newNames = TreeAutomaton . Map.map (renameDelta newNames) . delta
+
+renameDelta :: (Ord a, Ord b) => Map a b -> Map [Match a] (Action a) -> Map [Match b] (Action b)
+renameDelta newNames = Map.mapKeys (map (renameMatch newNames)) . Map.map (renameAction newNames)
+
+renameAction :: Ord a => Map a b -> Action a -> Action b
+renameAction newNames = \case
+    NextState a -> case Map.lookup a newNames of
+        Nothing -> undefined
+        Just b -> NextState b
+    Apply rule -> Apply rule
+
+renameMatch :: Ord a => Map a b -> Match a -> Match b
+renameMatch newNames = \case
+    State a -> case Map.lookup a newNames of
+        Nothing -> undefined
+        Just b -> State b
+    AnythingDifferent -> AnythingDifferent
